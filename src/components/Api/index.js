8eb108f7107049614ffa14ef940ad44a972abdd9
blackjmxx <alandou.jeanmichel@gmail.com>
2020-11-26 12:12:48 +0100

    fix

--- src/components/Api/index.js
+++ src/components/Api/index.js
@@ -1,5 +1,5 @@
 import { APIClient } from '@liskhq/lisk-api-client';
 
-const API_BASEURL = 'http://35.228.19.100/:4000';
+const API_BASEURL = 'http://35.228.19.100:4000';
 
 export const api = new APIClient([API_BASEURL]);
